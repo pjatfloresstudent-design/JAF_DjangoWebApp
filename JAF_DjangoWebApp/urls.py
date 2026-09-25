8a888e4f61adbb7ebecee559fd6fa66dacd4ed32
Prince John Andrew T. Flores <pjatflores.student@ua.edu.ph>
2026-09-25 19:22:58 +0800

    Add REST API authentication and session management

--- JAF_DjangoWebApp/urls.py
+++ JAF_DjangoWebApp/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/', include('registration.urls')),
+    path('api/', include('registration.api_urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
